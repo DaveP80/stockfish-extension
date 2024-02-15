74456a6d5653ed506e3afe5ead850607074f7e8c
davep80 <davidpaq1@gmail.com>
2024-02-14 23:08:42 -0500

    added route to evaluate a given fen position

--- README.md
+++ README.md
@@ -25,3 +25,16 @@
 
 1. Run `docker build -t stockfishapi:latest ./`
 2. Run `docker run -p 8080:8080 stockfishapi` ensuring the port is exposed.
+
+### Deployment
+
+```bash
+gcloud auth login
+export GCP_PROJECT_ID="xyz"
+export DOCKER_IMAGE_NAME="name"
+export GCR_REGION="us-east4"
+
+gcloud builds submit --tag gcr.io/${GCP_PROJECT_ID}/${DOCKER_IMAGE_NAME}:<tagname>
+
+gcloud run deploy <cloud_name> --image gcr.io/${GCP_PROJECT_ID}/${DOCKER_IMAGE_NAME}:<tagname>
+```
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from stockfish import Stockfish
+from pydantic import BaseModel
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -11,6 +12,8 @@ first_moves = [
     "1. e4", "1. d4", "1. Nf3", "1. Nc3", "1. Bc4", "1. Bf4", "1. g3", "1. b3", "1. f4", "1. c4",
     "1. a3", "1. a4", "1. b4", "1. c3", "1. d3", "1. e3", "1. f3", "1. h3", "1. h4", "1. Na3", "1. Nh3"
 ]
+
+STOCKFISH_PATH = os.getenv('STOCKFISH_PATH') or 'stockfish'
 
 DESCRIPTION = """
 This API does one thing only: it takes a chess game and returns the
@@ -82,7 +85,6 @@ def index():
 @app.get('/suggest-move/{gameid}', tags=['Chess Engine'])
 async def suggest_move(gameid: str):
 
-    STOCKFISH_PATH = os.getenv('STOCKFISH_PATH') or 'stockfish'
     usefen = scrape_kwdb_text(gameid)
     if len(usefen) == 0:
         return { "nodata": "unable to read fen or stockfish error" }
@@ -96,7 +98,7 @@ async def suggest_move(gameid: str):
     last_text = ""
     count = 0
     while True:
-        if count > 100:
+        if count > 5000:
             break
         text = stockfish._read_line()
         splitted_text = text.split(" ")
@@ -106,6 +108,37 @@ async def suggest_move(gameid: str):
             break
         last_text = text
         count += 1
-    if count > 100 or last_text == None:
+    if count > 4999 or last_text == None:
         return { "nodata": "unable to read fen or stockfish error" } 
-    return { "data": last_text }
+    return {"data": last_text}
+
+@app.get("/evaluation/")
+async def eval_fen(fen: str = Query(None)):
+    if fen:
+        try:
+            stockfish = Stockfish(path=STOCKFISH_PATH, parameters={"Threads": 3, "Ponder": "true"})
+            stockfish.set_depth(20)
+            fen.replace("%20", " ")
+
+            stockfish.set_fen_position(fen)
+            stockfish._go_time(1300)
+            last_text = ""
+            count = 0
+            while True:
+                if count > 5000:
+                    break
+                text = stockfish._read_line()
+                splitted_text = text.split(" ")
+                if splitted_text[0] == "bestmove":
+                    stockfish.info = last_text
+                    last_text = None if splitted_text[1] == "(none)" else " ".join(splitted_text)
+                    break
+                last_text = text
+                count += 1
+            if count > 4999 or last_text == None:
+                return { "nodata": "unable to read fen or stockfish error" } 
+            return {"data": last_text}
+        except:
+            return { "nodata": "bad fen string"}
+    else:
+        return {"message": "No query parameter provided"}
